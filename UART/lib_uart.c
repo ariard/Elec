2970f5cd1d3fbd3497f6aa4a36b4c5f6026e2291
AntoHesse <antoine.riard@gmail.com>
2017-05-23 21:39:51 +0200

    breakUART

--- UART/lib_uart.c
+++ UART/lib_uart.c
@@ -8,14 +8,32 @@ void	OpenUART(void)
 		// set 1 Stop bit
 		U1MODEbits.STSEL = 0;
 
-		//enable receiver
-		U1STAbits.URXEN = 1;
 		//enable transmitter
 		U1STAbits.UTXEN = 1;
+		//clear interrupt transmit
+		U1TXIFbits.UTXISEL = 10;
+
+		//enable receiver
+		U1STAbits.URXEN = 1;
+
+		// enable UART module 
+		U1MODEbits.ON = 1;
 }
 
 int		WriteUART(char *buffer)
 {
+	int	i = 0;:
+
+	U1STAbits.TRMT = 0;	
 	if (U1STAbits.UTXBF == 1)
 		return 1;
-		
+	while (i++ < 8)
+}
+								
+void		UARTSendBreak(void)
+{
+	if (U1STAbits.TRMT != 1)
+		return 1;		
+	U1STAbits.UTXBRK = 1;
+//	WRITE dummy on U1TXREG
+//	WRITE 0x55 on U1TXREG
