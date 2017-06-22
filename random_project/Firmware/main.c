5c6286f941d6c738b71df64a90b542a525d0a30e
ariard <ariard@student.42.fr>
2017-06-23 00:52:40 +0200

    Add files via upload

---
+++ random_project/Firmware/main.c
@@ -0,0 +1,138 @@
+#include <xc.h>
+#include <sys/attribs.h>
+#include "0x2a002a.h"
+
+#define INSTRUMENTS_COUNT       16
+#define PATTERNS_PER_INSTRUMENT 16
+#define QTIME_PER_INSTRUMENT    16
+#define NOTES_PER_QTIME         4
+#define ATTRIBUTES_PER_NOTE     3
+
+//  1 pattern = 192 u8
+//  1 instrument = 3072 u8
+
+u8	active_patterns[INSTRUMENTS_COUNT][QTIME_PER_INSTRUMENT][NOTES_PER_QTIME][ATTRIBUTES_PER_NOTE];
+u8	active_instrument[PATTERNS_PER_INSTRUMENT][QTIME_PER_INSTRUMENT][NOTES_PER_QTIME][ATTRIBUTES_PER_NOTE];
+u8      pattern_pointers[INSTRUMENTS_COUNT][PATTERNS_PER_INSTRUMENT];
+
+u8	cur_instrument;
+u8	cur_pattern;
+u8	cur_note;
+u8	cur_octave;
+u8	cur_velocity;
+u8	bpm;
+
+u8	encoders_memory[16][8];
+u8	encoders_play[16][8];
+//u16	notes_memory[16][16][16][4]; //trop grand pour etre dans la memoire mais a aller taper direct dans la flash
+u16	notes_play[16][16][4];
+u16	active_instruments;
+
+//	pour chaque instrument actif on ne garde que le pattern actif
+//	
+
+int main(void)
+{
+    u8  read;
+
+        GPIO_init();    // bien mettre a jour avec la vraie board
+        TIMER_init();
+	//I2C2_init();
+	SPI2_init();
+        //MCP_LCD_init();
+//        LCD_init();
+       // INT_init();
+        //HT16_init();
+        //MCP_ENCODERS_init();
+
+        SS_FLASH = 0;
+        SPI2BUF = 0x50;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SS_FLASH = 1;
+
+        SS_FLASH = 0;
+        SPI2BUF = 0x01;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0b00000000;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SS_FLASH = 1;
+
+        
+        SS_FLASH = 0;
+        SPI2BUF = 0x06;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SS_FLASH = 1;
+
+
+        SS_FLASH = 0;
+        SPI2BUF = 0x05;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x00;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SS_FLASH = 1;
+
+
+        
+        SS_FLASH = 0;
+        SPI2BUF = 0x02;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x00;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x00;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x02;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 42;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SS_FLASH = 1;
+
+        read = 10000;
+        while (read)
+        {
+            read --;
+        }
+
+        SS_FLASH = 1;
+        SS_FLASH = 0;
+        SPI2BUF = 0x03;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x00;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x00;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x02;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SPI2BUF = 0x00;
+        while (!SPI2STATbits.SPIRBF) ;
+        read = SPI2BUF;
+        SS_FLASH = 1;
+
+//        LCD_putstr(0, 0, "salut");
+//        LCD_putstr(1, 0, "bonjour");
+//        LCD_print_changed_chars();
+
+        T2CONbits.ON = 1;
+
+	while (42)
+	{
+//		manage_I2C2();
+		manage_SPI2();
+		WDTCONbits.WDTCLR = 1; // CLEAR WATCHDOG
+	}
+	return (0);
+}
